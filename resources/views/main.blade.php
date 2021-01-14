e46b754aee6dd19f6965e0b3be726041517681d6
pmh_projects <pawelmach@pm.me>
2021-01-14 21:14:58 +0100

    21:14

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -42,17 +42,24 @@
   <div
     class="p-5 text-center bg-image"
     style="
-      background-image: url('https://mdbootstrap.com/img/new/slides/041.jpg');
-      height: 400px;
+      background-image: url('https://mdbootstrap.com/img/new/slides/045.jpg');
+      height: 500px;
+      background-size: contain;
     "
   >
     <div class="mask" style="background-color: rgba(0, 0, 0, 0.6)">
       <div class="d-flex justify-content-center align-items-center h-100">
         <div class="text-white">
-          <h1 class="mb-3">Heading</h1>
-          <h4 class="mb-3">Subheading</h4>
+          <h1 class="mb-3">PHP</h1>
+          <h4 class="mb-3">Serwisy internetowe</h4>
           <a class="btn btn-outline-light btn-lg" href="#!" role="button"
-            >Call to action</a
+            >Dodaj</a
+          >
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+            >Usuń</a
+          >
+          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
+            >Edytuj</a
           >
         </div>
       </div>
